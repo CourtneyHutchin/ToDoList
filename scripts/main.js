d18d7a3e9f5082d0525ae43bcc029577b20bfb4a
CourtneyHutchin <courtneyhutchin47@gmail.com>
2019-10-22 14:50:57 -0700

    Adding and displaying todo item is functional

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,46 @@ var ToDoItem = /** @class */ (function () {
     return ToDoItem;
 }());
 var myItem = new ToDoItem("Finish class");
+window.onload = function () {
+    var addBtn = document.querySelector("form > input[type=button]");
+    addBtn.onclick = main;
+};
+function main() {
+    // Get ToDoItem from user
+    var item = getItem();
+    // Display new ToDoItem on page
+    displayToDoItem(item);
+    // Save ToDoItem
+}
+/**
+ * Move selected task to completed section of the web page
+ */
+function markAsComplete() {
+    var currItem = this;
+    var completedItems = document.getElementById("completed");
+    completedItems.appendChild(currItem);
+}
+/**
+ * Displays ToDoItem on the page
+ * @param item The item to be displayed
+ */
+function displayToDoItem(item) {
+    var div = document.createElement("div");
+    div.onclick = markAsComplete;
+    div.innerHTML = '<input type="checkbox">' + item.title;
+    console.log(div);
+    var displayDiv = document.getElementById("todo");
+    displayDiv.appendChild(div);
+}
+/**
+ * Gets the user input todo item
+ * from the form
+ */
+function getItem() {
+    var title = document.getElementById("title").value;
+    var item = new ToDoItem(title);
+    var deadline = document.getElementById("deadline").value;
+    item.deadline = new Date(deadline);
+    item.isCompleted = false;
+    return item;
+}
